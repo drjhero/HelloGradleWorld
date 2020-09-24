cbc17ceb5864c83061ed6e5b5688c46b93afe3af
jhero <james.thomas.herold@gmail.com>
2020-09-23 21:01:35 -0700

    Version 2

--- src/main/java/com/jhero/helloworld/Hello.java
+++ src/main/java/com/jhero/helloworld/Hello.java
@@ -3,6 +3,6 @@ package com.jhero.helloworld;
 public class Hello {
 
   public static void main(String[] args) {
-      System.out.println("Hello, Gradle world!");
+      System.out.println("Hello, Gradle world v2!");
   }
 }
